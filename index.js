b628d8f7b2fca06a97b18d8e52c6fb9a259bead1
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-02-25 17:08:18 -0300

    Transpiling code for new language features (babel)

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var moment = require('moment');
+import moment from 'moment';
 
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 module.exports = {
+    mode: 'development',
     entry: './index.js',
     output: {
         filename: 'bundle.js'
     },
-    mode: 'development'
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
 };
